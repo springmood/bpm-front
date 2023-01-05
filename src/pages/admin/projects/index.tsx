46dcdd5d5bb6b7db4489c187c4202c82190c125f
Amir Ehsan Haji Hussaini <niamileo@gmail.com>
2023-01-05 14:14:51 +0330

    using use-http in projects page

--- src/pages/admin/projects/index.tsx
+++ src/pages/admin/projects/index.tsx
@@ -1,29 +1,11 @@
 import { Box, SimpleGrid } from "@chakra-ui/react";
-import DevelopmentTable from "views/admin/dataTables/components/DevelopmentTable";
-import { columnsDataDevelopment } from "views/admin/dataTables/variables/columnsData";
-import tableDataDevelopment from "views/admin/dataTables/variables/tableDataDevelopment.json";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import AdminLayout from "layouts/admin";
-import { TableData } from "views/admin/default/variables/columnsData";
-import axios from "axios";
+import useFetch from "use-http";
+import { RenderJsonValues } from "components/utils/RenderJsonValues";
 
 export default function DataTables() {
-  const [projects, setProjects] = useState([]);
-  useEffect(() => {
-    axios
-      .get("http://localhost:8000/projects", {
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json",
-        },
-      })
-      .then((res) => {
-        setProjects(res.data.data);
-      })
-      .catch((e) => {
-        console.log("ERROR", e);
-      });
-  });
+  const { data = [] } = useFetch("http://localhost:8000/projects", {}, []);
   return (
     <AdminLayout>
       <Box pt={{ base: "130px", md: "80px", xl: "80px" }}>
@@ -32,17 +14,7 @@ export default function DataTables() {
           columns={{ sm: 1, md: 1 }}
           spacing={{ base: "20px", xl: "20px" }}
         >
-          {projects.map((project) => (
-            <div>
-              <h1>{project.id}</h1>
-              <h1>{project.title}</h1>
-              <h1>{project.desc}</h1>
-            </div>
-          ))}
-          {/* <DevelopmentTable
-            columnsData={columnsDataDevelopment}
-            tableData={projects as unknown as TableData[]}
-          /> */}
+          <RenderJsonValues>{data.data}</RenderJsonValues>
         </SimpleGrid>
       </Box>
     </AdminLayout>
