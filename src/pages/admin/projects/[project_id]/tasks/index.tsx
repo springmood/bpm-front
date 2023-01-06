6a8f0d223592589168974ebb8c37472fd775d826
Amir Ehsan Haji Hussaini <niamileo@gmail.com>
2023-01-06 13:53:16 +0330

    project tasks page added

---
+++ src/pages/admin/projects/[project_id]/tasks/index.tsx
@@ -0,0 +1,35 @@
+import { Box, Button, SimpleGrid } from "@chakra-ui/react";
+import React from "react";
+import AdminLayout from "layouts/admin";
+import useFetch from "use-http";
+import DataTable from "components/data/DataTable";
+import Link from "next/link";
+
+export default function ProjectTasks() {
+  const { data, loading } = useFetch("http://localhost:8000/tasks", {}, []);
+
+  return (
+    <AdminLayout>
+      <Box pt={{ base: "130px", md: "80px", xl: "80px" }}>
+        <SimpleGrid
+          mb="20px"
+          columns={{ sm: 1, md: 1 }}
+          spacing={{ base: "20px", xl: "20px" }}
+        >
+          <Link href={"/admin/projects/form"}>
+            <Button>Add New</Button>
+          </Link>
+          <DataTable
+            loading={loading}
+            columns={[
+              { label: "id", name: "id" },
+              { label: "title", name: "title" },
+              { label: "desc", name: "desc" },
+            ]}
+            rows={data?.data}
+          />
+        </SimpleGrid>
+      </Box>
+    </AdminLayout>
+  );
+}
