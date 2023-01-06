ac68ae8865d721b92de06c215c01cb53901093f8
Amir Ehsan Haji Hussaini <niamileo@gmail.com>
2023-01-06 11:28:19 +0330

    using new DataTable Component in projects form

--- src/pages/admin/projects/index.tsx
+++ src/pages/admin/projects/index.tsx
@@ -1,11 +1,13 @@
-import { Box, SimpleGrid } from "@chakra-ui/react";
+import { Box, Button, SimpleGrid } from "@chakra-ui/react";
 import React from "react";
 import AdminLayout from "layouts/admin";
 import useFetch from "use-http";
-import { RenderJsonValues } from "components/utils/RenderJsonValues";
+import DataTable from "components/data/DataTable";
+import Link from "next/link";
 
 export default function DataTables() {
-  const { data = [] } = useFetch("http://localhost:8000/projects", {}, []);
+  const { data } = useFetch("http://localhost:8000/projects", {}, []);
+
   return (
     <AdminLayout>
       <Box pt={{ base: "130px", md: "80px", xl: "80px" }}>
@@ -14,7 +16,17 @@ export default function DataTables() {
           columns={{ sm: 1, md: 1 }}
           spacing={{ base: "20px", xl: "20px" }}
         >
-          <RenderJsonValues>{data.data}</RenderJsonValues>
+          <Link href={"/admin/projects/form"}>
+            <Button>Add New</Button>
+          </Link>
+          <DataTable
+            columns={[
+              { label: "id", name: "id" },
+              { label: "title", name: "title" },
+              { label: "desc", name: "desc" },
+            ]}
+            rows={data?.data}
+          />
         </SimpleGrid>
       </Box>
     </AdminLayout>
